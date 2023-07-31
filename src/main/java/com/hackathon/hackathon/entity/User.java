ca36ba039ab4bfdee30673b6c5f4ea359362d17b
lydbs <lydbsdud@gmail.com>
2023-07-31 10:13:51 +0900

    yun

---
+++ src/main/java/com/hackathon/hackathon/entity/User.java
@@ -0,0 +1,30 @@
+package com.hackathon.hackathon.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    // 추가적인 필드와 메서드를 정의할 수 있음
+}
